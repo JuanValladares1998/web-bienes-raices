2e6f55f9d56f9d9746a55c36298f3c596dec86c6
Juan Valladares <valladaresjj2014@gmail.com>
2022-10-09 13:51:57 -0500

    se agrego el dark mode al local storage

--- src/js/app.js
+++ src/js/app.js
@@ -6,24 +6,52 @@ document.addEventListener('DOMContentLoaded', () => {
 function darkMode() {
     const preferenciaDarkMode = window.matchMedia('(prefers-color-scheme: dark');
 
-    if (preferenciaDarkMode.matches === true) {
-        document.body.classList.add('dark-mode');
+    let dark_mode_local_storage = localStorage.getItem("dark_mode");
+
+    //comprobar si la variable DARKMODE esta creada en el local storage
+
+    //si no esta creada leemos las preferencias del sistema
+    if (dark_mode_local_storage === null) {
+
+        if (preferenciaDarkMode.matches === true) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('dark_mode', 'true');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('dark_mode', 'false');
+        }
     } else {
-        document.body.classList.remove('dark-mode');
+        if (dark_mode_local_storage === 'true') {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+        console.log('hay datos en el local storage');
     }
 
-    preferenciaDarkMode.addEventListener('change', () => {
+    //Cambiar el darkmode dependiendo de las preferencias del sistema
+
+    /*preferenciaDarkMode.addEventListener('change', () => {
         if (preferenciaDarkMode.matches === true) {
             document.body.classList.add('dark-mode');
         } else {
             document.body.classList.remove('dark-mode');
         }
-    });
+    });*/
 
-    //cambiar a modo oscuro
+    //cambiar a modo oscuro mediante el boton
     const botonDarkMode = document.querySelector('.dark-mode-boton');
+
     botonDarkMode.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+
+        if (document.body.classList.contains('dark-mode') === true) {
+            localStorage.setItem('dark_mode', 'true');
+            console.log('modo claro');
+        } else {
+            localStorage.setItem('dark_mode', 'false');
+            console.log('modo oscuro');
+        }
     })
 }
 
